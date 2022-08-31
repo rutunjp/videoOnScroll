8c08678d124c7a946fc50fa4afcc7ce18332b304
Rutunj <rutunjp206@gmail.com>
2022-08-31 18:55:50 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const intro = document.querySelector('.intro')
+const video = intro.querySelector('video')
+const text = intro.querySelector('h1')
+//END section
+const section = document.querySelector('section')
+const end = section.querySelector('h1')
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller()
+
+let scene = new ScrollMagic.Scene({
+  duration: 9000,
+  triggerElement: intro,
+  triggerHook: 0,
+})
+  .addIndicators()
+  .setPin(intro)
+  .addTo(controller)
+
+//Text Animation
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 })
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0,
+})
+  .setTween(textAnim)
+  .addTo(controller)
+
+//Video Animation
+let accelamount = 1
+let scrollpos = 100
+let delay = 0
+
+scene.on('update', (e) => {
+  scrollpos = e.scrollPos / 1000
+})
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount
+  console.log(scrollpos, delay)
+
+  video.currentTime = delay
+}, 60)
